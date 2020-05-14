97e72e65dd9ea1667d5f5d31470233b29d0bb0f4
Lukasz Bekus <beckon@interia.pl>
2020-05-14 09:32:11 +0200

    dodane api zamiast fake

--- src/shared/data.js
+++ src/shared/data.js
@@ -2,10 +2,10 @@ import * as axios from 'axios';
 
 const getTeams = async function(){
     try {
-    const response = await axios.get('./api/teams.json');
-    const teams = response.data;
-    if (!response.data) return [];
-    return teams;
+      const response = await axios.get('https://localhost:5001/api/teams');
+      const teams = response.data;
+      if (!response.data) return [];
+      return teams;
     }
     catch (error) {
     console.error(error);
@@ -15,10 +15,9 @@ const getTeams = async function(){
 
 const getTeam = async function(id){
   try {
-    const response = await axios.get('../api/teams.json');
-    const teams = response.data;
-    const team = teams.find(t=>t.id === id);
-    if (!team) return console.log("zlo");
+    const response = await axios.get(`https://localhost:5001/api/teams/${id}`);
+    const team = response.data;
+    if (!team) return console.log("bład pusta druzyna");
     return team;
   } catch (error) {
     console.error(error);
