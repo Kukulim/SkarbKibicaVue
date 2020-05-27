b204512fc46ce26278539627f0abfc6ac551d6ce
kukulim <beckon@interia.pl>
2020-05-27 14:49:42 +0200

    vuex install + config

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import "bootstrap";
 import router from './router'
+import store from './store'
 
 import VuejsDialog from "vuejs-dialog"
 import 'vuejs-dialog/dist/vuejs-dialog.min.css';
@@ -39,5 +40,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
