672a21c2f22ca6fd7db6b8c5de1d636e0caf9d6b
kukulim <beckon@interia.pl>
2020-10-21 10:34:32 +0200

    test

--- src/shared/data.js
+++ src/shared/data.js
@@ -1,6 +1,6 @@
 import * as axios from 'axios';
 
-const server = "https://skar.kibica.api.hostingasp.pl/";
+const server = "skar.kibica.api.hostingasp.pl/";
 
 const getTeams = async function(){
     try {
